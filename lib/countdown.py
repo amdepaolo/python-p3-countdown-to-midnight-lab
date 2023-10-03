27af6dd68f85b46de1bba3bb7421d2267285eeee
Adam DePaolo <amdepaolo@gmail.com>
2023-10-02 21:58:46 -0400

    passes tests

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+import time 
+
+def countdown(seconds):
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        seconds -= 1
+    if seconds == 0:
+        print("HAPPY NEW YEAR!")
+        return
+    
+def countdown_with_sleep(seconds):
+    while seconds > 0:
+        print(f"{seconds} SECOND(S)!")
+        seconds -= 1
+        time.sleep(1)
+    if seconds == 0:
+        print("HAPPY NEW YEAR!")
+        return
